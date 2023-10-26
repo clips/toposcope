2b5936898e2224ad002a041c202ab2f23c5365d5
LemmensJens <jenslemmens97@mail.com>
2023-10-26 14:10:23 +0000

    reset config

--- create_config.py
+++ create_config.py
@@ -34,15 +34,15 @@ config_object["TOP2VEC_CONFIG"] = {
     "tokenize": 1, # 0 (no) or 1 (yes), must be 1 if 'lemmatize' is 1 or will result in error
     "lemmatize": 0, # 0 (no) or 1 (yes)
     "remove_nltk_stopwords": 1, # 0 (no) or 1 (yes), removes NLTK's default stopwords, see 'lang' config parameter
-    "remove_custom_stopwords": './seed_topics.txt', # if not empty: full path to .txt file with 1 stopword per line
+    "remove_custom_stopwords": '', # if not empty: full path to .txt file with 1 stopword per line
     "remove_punct": 1, # 0 (no) or 1 (yes)
     "lowercase": 1, # 0 (no) or 1 (yes)
-    "upper_ngram_range": 2, #upper ngram range for keywords, creates ngrams for range (1, n)
+    "upper_ngram_range": 1, #upper ngram range for keywords, creates ngrams for range (1, n)
     "lang": 'english' # 'dutch', 'english', 'french', 'german'; relevant for stopwords, tokenization, lemmatization, embedding model selection
 }
 
 config_object["LDA_CONFIG"] = {
-    "n_components": 10, # number of topics to detect
+    "n_components": 20, # number of topics to detect
     "preprocess": 1, # 0 (no) or 1 (yes)
     "tokenize": 1, # 0 (no) or 1 (yes), must be 1 if 'lemmatize' is 1 or will result in error
     "lemmatize": 0, # 0 (no) or 1 (yes)
