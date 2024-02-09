41f8f7bbec90b1bc8c221d6bf6b18c8fb3e0dcae
LemmensJens <jenslemmens97@mail.com>
2024-02-09 15:29:08 +0000

    randomize output filename

--- topic_modeling_app.py
+++ topic_modeling_app.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, uuid
 import spacy
 import pandas as pd
 import random as rd
@@ -14,6 +14,8 @@ rd.seed(42)
 
 def main(file, lang, algorithm, preprocessing_steps, model, min_topic_size, timestamp_col, n_topics, upper_ngram_range, progress=gr.Progress(track_tqdm=True)):
     
+    unique_output_id = str(uuid.uuid4())
+
     min_topic_size = int(min_topic_size)
     n_topics = int(n_topics)
     upper_ngram_range = int(n_topics)
@@ -75,31 +77,30 @@ def main(file, lang, algorithm, preprocessing_steps, model, min_topic_size, time
         )
     
 #PREPARE_OUTPUT_DIR____________________________________________________________________________
-    dir_out = 'output'
+    # first check if directory where all outputs are stored exists
+    main_dir_out = 'outputs'
+    if not os.path.exists(main_dir_out):
+        os.mkdir(main_dir_out)
 
-    # if overwrite_output_dir is True, delete the directory
-    # else, check if output dir exists already and return error if it does
-    # create the output directory
-    if os.path.exists(dir_out):
-        shutil.rmtree(dir_out)
-    else:
-        assert os.path.exists(dir_out) == False
-
-    os.mkdir(dir_out)
-    os.mkdir(os.path.join(dir_out, 'visualizations'))
+    # then create unique output dir for process
+    unique_dir_out = os.path.join(main_dir_out, unique_output_id)
+    if os.path.exists(unique_dir_out): # this should not happen in theory
+        shutil.rmtree(unique_dir_out)
+    os.mkdir(unique_dir_out)
+    os.mkdir(os.path.join(unique_dir_out, 'visualizations'))
 
 #FIT_MODEL_____________________________________________________________________________________
     if algorithm == 'BERTopic':
-        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = BERT_topic(df, model, 'text', dir_out, lang, upper_ngram_range, min_topic_size, n_topics, input_format, timestamps)
+        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = BERT_topic(df, model, 'text', unique_dir_out, lang, upper_ngram_range, min_topic_size, n_topics, input_format, timestamps)
     
     elif algorithm == 'LDA':
-        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = LDA_model(df, 'text', dir_out, upper_ngram_range, n_topics, input_format, timestamps)
+        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = LDA_model(df, 'text', unique_dir_out, upper_ngram_range, n_topics, input_format, timestamps)
     
     elif algorithm == 'NMF':
-        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = NMF_model(df, 'text', dir_out, upper_ngram_range, n_topics, input_format, timestamps)
+        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = NMF_model(df, 'text', unique_dir_out, upper_ngram_range, n_topics, input_format, timestamps)
     
     elif algorithm == 'Top2Vec':
-        topic_doc_matrix, keyword_df, topic_term_matrix,doc_plot = top_2_vec(df, 'text', model, dir_out, n_topics, input_format, timestamps)
+        topic_doc_matrix, keyword_df, topic_term_matrix, doc_plot = top_2_vec(df, 'text', model, unique_dir_out, n_topics, input_format, timestamps)
 
     keywords = keyword_df.keywords.tolist()
 
@@ -118,21 +119,21 @@ def main(file, lang, algorithm, preprocessing_steps, model, min_topic_size, time
         'diversity': [diversity],
         'coherence': [coherence_score]
     })
-    eval_df.to_csv(os.path.join(dir_out, 'evaluation.csv'), index=False)
+    eval_df.to_csv(os.path.join(unique_dir_out, 'evaluation.csv'), index=False)
     
     #KEYWORDS PER TOPIC
-    keyword_df.to_csv(os.path.join(dir_out, 'keywords_per_topic.csv'), index=False)
+    keyword_df.to_csv(os.path.join(unique_dir_out, 'keywords_per_topic.csv'), index=False)
 
     #TOPIC-TERM MATRIX
     topic_term_matrix.sort_index(axis=1, inplace=True)
-    topic_term_matrix.to_csv(os.path.join(dir_out, 'topic_term_matrix.csv'))
+    topic_term_matrix.to_csv(os.path.join(unique_dir_out, 'topic_term_matrix.csv'))
 
     #TOPIC-DOC MATRIX
     idx_column = topic_doc_matrix['idx']
     topic_doc_matrix = topic_doc_matrix.drop(columns=['idx'])
     topic_doc_matrix.sort_index(axis=1, inplace=True)
     topic_doc_matrix = pd.concat([idx_column, topic_doc_matrix], axis=1)
-    topic_doc_matrix.to_csv(os.path.join(dir_out, 'topic_doc_matrix.csv'), index=False)
+    topic_doc_matrix.to_csv(os.path.join(unique_dir_out, 'topic_doc_matrix.csv'), index=False)
 
     #ANNOTATIONS
     idx_column = topic_doc_matrix['idx']
@@ -140,11 +141,14 @@ def main(file, lang, algorithm, preprocessing_steps, model, min_topic_size, time
     label_column = topic_doc_matrix.apply(lambda row: row.idxmax(), axis=1)
     label_column.name = 'main topic'
     topic_doc_matrix = pd.concat([idx_column, label_column], axis=1)
-    topic_doc_matrix.to_csv(os.path.join(dir_out, 'annotations.csv'), index=False)
+    topic_doc_matrix.to_csv(os.path.join(unique_dir_out, 'annotations.csv'), index=False)
 
     print('Done!')
 
-    return shutil.make_archive(base_name='output', format='zip', base_dir='output', root_dir='.')
+    return (
+        shutil.make_archive(base_name=os.path.join(unique_dir_out), format='zip', base_dir=unique_dir_out),
+        doc_plot,
+    )
 #______________________________________________________________________________________________
 if __name__ == "__main__":
     main()
