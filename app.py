1ff495c884edaefc2efbc8cbf2a87d45214504f8
Jens Lemmens <55537395+LemmensJens@users.noreply.github.com>
2024-06-05 16:40:41 +0200

    activate link sharing for colab

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ In order to help the user evaluate the topic model, diversity and coherence scor
         gr.Markdown("""<center><img src="https://thomasmore.be/sites/default/files/2022-11/UA-hor-1-nl-rgb.jpg" alt="Image" width="175"/></center>""")
 
 
-demo.launch(share=False, server_port=7861, server_name='0.0.0.0')
+demo.launch(share=True, server_port=7861, server_name='0.0.0.0')
